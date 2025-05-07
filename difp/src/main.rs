cc85ce05efeb1aa08ad8239da900c372d985c2cb
Joe <joseph.winfield@legrand.com>
2025-05-07 16:27:41 -0500

    use file_io in main.rs

--- difp/src/main.rs
+++ difp/src/main.rs
@@ -1,10 +1,17 @@
 use clap::Parser;
 
+mod file_io;
+use file_io::discovery::{get_files_and_directories, is_directory, is_file};
 mod cli;
 use cli::scan_args::ScanArgs;
 
 fn main() {
-    println!("Hello, world!");
     let args = ScanArgs::parse();
-    println!("{}", args);
+    if is_directory(args.get_path()) {
+        get_files_and_directories(args.get_path());
+    } else if is_file(args.get_path()) {
+       println!("{} is a file", args.get_path()); 
+    } {
+       println!("some error must've snuck through");
+    }
 }
