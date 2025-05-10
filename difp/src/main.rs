16498ae903cda6c883243685cd1b972c69d51768
Joe <joseph.winfield@legrand.com>
2025-05-09 20:12:14 -0500

    update main.rs

--- difp/src/main.rs
+++ difp/src/main.rs
@@ -1,14 +1,14 @@
 use clap::Parser;
-
+mod git_io;
 mod file_io;
-use file_io::discovery::{get_files_and_directories, is_directory, is_file};
+use file_io::discovery::{is_directory, is_file};
 mod cli;
 use cli::scan_args::ScanArgs;
 
 fn main() {
     let args = ScanArgs::parse();
     if is_directory(args.get_path()) {
-        get_files_and_directories(args.get_path());
+        println!("{} is a directory", args.get_path());
     } else if is_file(args.get_path()) {
        println!("{} is a file", args.get_path()); 
     } {
